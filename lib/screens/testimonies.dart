4d612c159cf76892d27d8f947756dedec3b226c7
Israel B Lewis <israellewis1@outlook.com>
2022-03-20 00:58:16 +0300

    Work on Testimony and firebase.

--- lib/screens/testimonies.dart
+++ lib/screens/testimonies.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
+import 'package:kin/bloc/testimony_bloc.dart';
 import 'package:kin/models/testimony_model.dart';
-
-import '../constants/constants.dart';
+import 'package:provider/provider.dart';
+import 'package:kin/constants/constants.dart';
 
 class Testimonies extends StatelessWidget {
   const Testimonies({Key? key}) : super(key: key);
@@ -24,14 +25,33 @@ class Testimonies extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-
-      body: ListView(
-        children: testimonies
-            .map(
-              (testimony) => Padding(
+      body: StreamBuilder<List<QueryDocumentSnapshot<TestimonyModel>>>(
+        stream: context.read<TestimonyBloc>().getAllTestimonies(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Error in Testimonies'),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: kGold,
+              ),
+            );
+          }
+          if (snapshot.data!.isEmpty) {
+            return const Center(child: Text('Testimonies will be uploaded'));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: ((context, index) {
+              final testimony = snapshot.data![index].data();
+              return Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
-                  padding: const EdgeInsets.all(10),
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
                   decoration: BoxDecoration(
                     gradient: LinearGradient(colors: [
                       kGold,
@@ -43,27 +63,28 @@ class Testimonies extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        testimony.name,
+                        testimony.name!,
                         style: const TextStyle(
-                          fontWeight: FontWeight.bold,
                           fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                       const SizedBox(
                         height: 10,
                       ),
                       Text(
-                        testimony.testimony,
+                        testimony.testimony!,
                         style: const TextStyle(
-                          fontSize: 18,
+                          fontSize: 16,
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            )
-            .toList(),
+              );
+            }),
+          );
+        },
       ),
     );
   }
